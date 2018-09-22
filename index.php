a0110fb68dd522d999727423283587d14214fd25
Николай Никишин <36757507+NicolaiNikishin@users.noreply.github.com>
2018-09-23 02:51:04 +0300

    Тестирую

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
 
 
-/**
- *
- */
-class ClassName extends AnotherClass
-{
-
-  function __construct(argument)
-  {
-    // code...
-  }
-
-  public function FunctionName($value='')
-  {
-    // code...
-
-
-    echo "string";
-  }
-
-}
+echo "string";
